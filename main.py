202fd9b130285b6e5451fd67e7437da75e7a8b66
arslanAta <arsyakmar@gmail.com>
2025-04-12 10:15:59 +0500

    Added try exception

--- main.py
+++ main.py
@@ -1,68 +1,75 @@
-from PyPDF2 import PdfReader, PdfWriter
-from pathlib import Path
+from PyPDF2 import PdfReader,PdfWriter,errors
+from pathlib import  Path
 
 def encrypt():
-    file_path = input("[+] Enter file path: ").strip()
+    file_path = input("[+] Enter file path: ")
     file = Path(file_path)
 
     if not file.exists():
-        print("[!] File not found. Try again")
+        print('[!] File not exist in given path. Try again')
         return
 
-    encrypt_password = input("[+] Enter password for encrypt: ")
-    new_file_name = input("[+] Enter new encrypted file name: ")
-
-    writer = PdfWriter()
-    try:
-        reader = PdfReader(file_path)
-
-        for page in range(len(reader.pages)):
-            writer.add_page(reader.pages[page])
-
-        writer.encrypt(encrypt_password)
-        with open(new_file_name, 'wb') as file:
-            writer.write(file)
-
-        print(f"[+] Created {new_file_name}")
-    except Exception as e:
-        print(f"[!] Error: {e}")
-
-
-def decrypt():
-    file_path = input("[+] Enter file path: ").strip()
-    file = Path(file_path)
-
-    if not file.exists():
-        print("[!] File not found. Try again")
-        return
-
-    decrypt_password = input("[+] Enter password for decrypt: ")
+    password = input("[+] Enter encryption password: ")
+    while len(password)==0:
+        password = input("[+] Enter encryption password: ")
     new_file_name = input("[+] Enter new file name: ")
 
+    reader = PdfReader(file_path)
+    writer = PdfWriter()
+
+    for page in range(len(reader.pages)):
+        writer.add_page(reader.pages[page])
+
+    writer.encrypt(password)
+
+    print(f"[*] Writing to {new_file_name}...")
+    with open(new_file_name,'wb') as file:
+        writer.write(file)
+        print(f"[+] Created {new_file_name}")
+
+def decrypt():
+    file_path = input("[+] Enter file path: ")
+    file = Path(file_path)
+
+    if not file.exists():
+        print("[!] File not exists in given path. Try again")
+        return
+
+    password = input("[+] Enter password for decryption: ")
+    new_file_name = input("[+] Enter new file name: ")
+
+    reader = PdfReader(file_path)
     writer = PdfWriter()
 
     try:
-        reader = PdfReader(file_path)
-        if reader.is_encrypted:
-            reader.decrypt(decrypt_password)
+        result = reader.decrypt(password)
+        if result == 0:
+            print("[!] Incorrect password.")
+            return
+    except errors.FileNotDecryptedError:
+        print("[!] Error decrypting the file.")
+        return
 
-        for page in range(len(reader.pages)):
-            writer.add_page(reader.pages[page])
+    for page in range(len(reader.pages)):
+        writer.add_page(reader.pages[page])
 
-        with open(new_file_name, 'wb') as file:
-            writer.write(file)
-    except Exception as e :
-        print(f"[!] Error: {e}")
+    print(f"[*] Writing to {new_file_name}...")
+    with open(new_file_name,'wb') as file:
+        writer.write(file)
+        print(f"[+] Created {new_file_name}")
+
 
 def main():
-    choose = input("[+] Enter 0 for encrypt 1 for decrypt file: ").strip()
+    choose = input("[INFO] Enter 0 to encrypt 1 to decrypt file: ").strip()
     match choose:
         case "1":
             decrypt()
         case "0":
             encrypt()
+        case "quit":
+            return
         case _:
-            print("[!] Unknown command. Try again")
+            print("[!] Enter correct key")
 
 if __name__ == "__main__":
     main()
