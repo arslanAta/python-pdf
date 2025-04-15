4c768dab5dbdefd1ae01f55b082e92d4d63b15ac
arslanAta <arsyakmar@gmail.com>
2025-04-15 20:26:48 +0500

    Created GUI

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
-from PyPDF2 import PdfReader,PdfWriter,errors
-from pathlib import  Path
+from pathlib import Path
+from PyPDF2 import PdfReader, PdfWriter, errors
 
-def encrypt():
-    file_path = input("[+] Enter file path: ")
+import tkinter as tk
+from tkinter import filedialog, simpledialog, messagebox
+
+import ttkbootstrap as tb
+from ttkbootstrap import  Querybox
+from ttkbootstrap.dialogs import Messagebox
+
+
+def encrypt(file_path,password):
     file = Path(file_path)
 
     if not file.exists():
         print('[!] File not exist in given path. Try again')
         return
 
-    password = input("[+] Enter encryption password: ")
-    while len(password)==0:
-        password = input("[+] Enter encryption password: ")
-    new_file_name = input("[+] Enter new file name: ")
-
+    new_file_name = f"encrypted_{file.name}"
     reader = PdfReader(file_path)
     writer = PdfWriter()
 
@@ -26,6 +29,8 @@ def encrypt():
     with open(new_file_name,'wb') as file:
         writer.write(file)
         print(f"[+] Created {new_file_name}")
+        Messagebox.ok("File encrypted successfully!", title="Success", alert=True)
+
 
 def decrypt():
     file_path = input("[+] Enter file path: ")
@@ -57,19 +62,32 @@ def decrypt():
     with open(new_file_name,'wb') as file:
         writer.write(file)
         print(f"[+] Created {new_file_name}")
+        messagebox.showinfo("Success", f"File: {file_path}\nPassword: {password}", parent=window)
 
+def select_file():
+    file_path = filedialog.askopenfilename(filetypes=[('PDF','.pdf')])
+    if file_path:
+        password = Querybox.get_string("Enter password for encryption", title="Password")
+        if password:
+
+            encrypt(file_path, password)
 
 def main():
-    choose = input("[INFO] Enter 0 to encrypt 1 to decrypt file: ").strip()
-    match choose:
-        case "1":
-            decrypt()
-        case "0":
-            encrypt()
-        case "quit":
-            return
-        case _:
-            print("[!] Enter correct key")
+    window.title("Encrypt && Decrypt PDF")
+    window.geometry('400x200')
 
+    btn = tb.Button(
+        window,
+        text="Select File to Encrypt",
+        bootstyle="primary",  # Correct bootstyle usage
+        width=30,
+        command=select_file,
+        padding=(10, 15),
+    )
+    btn.pack(expand=True)
+
+    window.mainloop()
+
+window = tk.Tk()
 if __name__ == "__main__":
     main()
